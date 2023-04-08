cf802f9568de56d9b505728ee270c7abd067d3dd
LEXDEIT <godhood@proton.me>
2023-04-07 23:45:48 -0600

    UI Gradients added

--- components/Portafolio/portfolio.jsx
+++ components/Portafolio/portfolio.jsx
@@ -1,5 +1,7 @@
-import { motion, useViewportScroll, useTransform } from "framer-motion";
+import { motion } from "framer-motion";
 import styles from './portfolio.module.css';
+import { useState } from "react";
+
 
 const tituloAnimation = {
     initial: {
@@ -18,8 +20,7 @@ const tituloAnimation = {
 
 
 const Portfolio = () => {
-    const { scrollYProgress } = useViewportScroll();
-    const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
+
 
 
     return (
@@ -43,24 +44,9 @@ const Portfolio = () => {
                         viewport={{ once: true }}
                     >by {`{LexdeIT}`}</motion.h3>
 
-
-
                     <div className={styles.cajon}>
 
                     </div>
-
-                    <div className={styles.wrapper}>
-                        <motion.div
-                            className={styles.container}
-                            style={{ scale, opacity: 0 }}
-                        >
-                            <motion.div
-                                className={styles.item}
-                                style={{ scaleY: scrollYProgress, opacity: 1 }}
-                            />
-                        </motion.div>
-                    </div>
-
                 </div>
             </section>
         </>
--- components/Profile/profile.jsx
+++ components/Profile/profile.jsx
@@ -1,12 +1,12 @@
 import { motion } from 'framer-motion';
 import styles from "../Profile/profile.module.css";
 import { useState, useEffect } from "react";
-import Spline from "@splinetool/react-spline";
 
 
 
 const Profile = () => {
   const [isScreenBig, setIsScreenBig] = useState(false);
+
 
   const handleResize = () => {
     setIsScreenBig(window.innerWidth >= 1200);
@@ -18,13 +18,37 @@ const Profile = () => {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
+  const imageVariants = {
+    //Estado inicial
+    initial: {
+      opacity: 0,
+      x: 500,
+      borderRadius: '50%',
+      width: '5em',
+      height: '5em',
+    },
+    //Animacion que va a realizar
+    animate: {
+      opacity: 1,
+      x: 0,
+      width: '20em',
+      height: '20em',
+      //Transicion
+      transition: {
+        type: 'spring',
+        duration: 1.5,
+        delay: 1.3
+      }
+
+    }
+  }
 
 
   return (
     <>
       <section id="profile">
 
-        {isScreenBig && <iframe className={styles.iframer} src="https://www.youtube.com/embed/RhlQvbvMg-0?&autoplay=1&loop=1&mute=1&showinfo=0&controls=0"></iframe>}
+        {isScreenBig && <iframe className={styles.iframer} src="https://www.youtube.com/embed/RhlQvbvMg-0?&autoplay=1&loop=1&mute=1&showinfo=0&controls=0" />}
         <div className={styles.contenedor}>
 
           <div className={styles.subcontenedor}>
@@ -40,26 +64,27 @@ const Profile = () => {
               >Programador <br /> {`{Full Stack}`} </motion.h1>
 
               <motion.h6
-                initial={{ opacity: 0, y: 100 }}
+                initial={{ opacity: 0, y: 100,  }}
+                whileInView={{ opacity: 1, y: 0 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.8, delay: 0.7 }}
               >Tecnologias de vanguardia</motion.h6>
 
             </div>
 
-
-
-
             <>
-              <Spline scene="https://prod.spline.design/g9q8B7AyJ3VvEy1a/scene.splinecode" />
-
+              <motion.img
+                src='./profile.webp'
+                variants={imageVariants}
+                initial='initial'
+                animate='animate'
+                transition='transition'
+                alt="Emmanuel Villavicencio"
+                priority
+              />
 
 
             </>
-
-
-
-
 
           </div>
         </div>
